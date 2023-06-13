c7391ab084ff743e8547b324791cca6b454824c9
Ronak Jeengar <ronakjeengar7860@gmail.com>
2023-06-13 17:40:53 +0530

    Added ViewBinding

--- app/src/main/java/com/ronakJ/topnews/DetailWebView.kt
+++ app/src/main/java/com/ronakJ/topnews/DetailWebView.kt
@@ -6,29 +6,35 @@ import android.os.Bundle
 import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import kotlinx.android.synthetic.main.activity_detail_web_view.*
+import com.ronakJ.topnews.databinding.ActivityDetailWebViewBinding
 
 class DetailWebView : AppCompatActivity() {
+
+    private var binding: ActivityDetailWebViewBinding? = null
+
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_web_view)
+        binding = ActivityDetailWebViewBinding.inflate(layoutInflater)
+        val view = binding?.root
+        setContentView(view)
 
-        val url : String? = intent.getStringExtra("URL")
-        if (url != null){
+        val url: String? = intent.getStringExtra("URL")
+        if (url != null) {
 
-            detailWebView.settings.javaScriptEnabled = true
-            detailWebView.settings.userAgentString = "Mozilla/5.0 (iPhone; U; CPU like Mac OS X; en) AppleWebKit/420+ (KHTML, like Gecko) Version/3.0 Mobile/1A543a Safari/419.3"
+            binding?.detailWebView?.settings!!.javaScriptEnabled = true
+            binding?.detailWebView?.settings!!.userAgentString =
+                "Mozilla/5.0 (iPhone; U; CPU like Mac OS X; en) AppleWebKit/420+ (KHTML, like Gecko) Version/3.0 Mobile/1A543a Safari/419.3"
 
-            detailWebView.webViewClient = object : WebViewClient(){
+            binding?.detailWebView?.webViewClient = object : WebViewClient() {
 
                 override fun onPageFinished(view: WebView?, url: String?) {
                     super.onPageFinished(view, url)
-                    progressBar.visibility = View.GONE
-                    detailWebView.visibility = View.VISIBLE
+                    binding?.progressBar?.visibility = View.GONE
+                    binding?.detailWebView?.visibility = View.VISIBLE
                 }
             }
-            detailWebView.loadUrl(url)
+            binding?.detailWebView?.loadUrl(url)
         }
     }
 }
